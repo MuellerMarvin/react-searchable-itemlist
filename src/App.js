e68dd1527a8b8029287380ff53b5cd1e7ea8f32b
MuellerMarvin <marvin.mueller@code.berlin>
2019-10-30 22:31:32 +0100

    Title-limits and letter-spacing

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends React.Component {
     let itemList = [];
 
     itemList.push(
-      <ListItem Title="Title" Description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse placerat pulvinar ante pretium pretium. Quisque mollis a urna ut accumsan. Maecenas ut mattis tellus. Cras eu congue metus, sit amet pulvinar nunc. Cras dignissim lacus nec bibendum bibendum. Sed ipsum ex, imperdiet nec metus vitae, maximus ultrices metus. Duis posuere pellentesque nibh vitae molestie. Donec ullamcorper bibendum tempor. Nunc massa leo, placerat in nisi vitae, bibendum pellentesque eros. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae." ImageSource="https://upload.wikimedia.org/wikipedia/commons/e/ed/20170522_Schalkenmehrener_Maar_30.jpg"></ListItem>
+      <ListItem Title="Example Title" Description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse placerat pulvinar ante pretium pretium. Quisque mollis a urna ut accumsan. Maecenas ut mattis tellus. Cras eu congue metus, sit amet pulvinar nunc. Cras dignissim lacus nec bibendum bibendum. Sed ipsum ex, imperdiet nec metus vitae, maximus ultrices metus. Duis posuere pellentesque nibh vitae molestie. Donec ullamcorper bibendum tempor. Nunc massa leo, placerat in nisi vitae, bibendum pellentesque eros. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae." ImageSource="https://upload.wikimedia.org/wikipedia/commons/e/ed/20170522_Schalkenmehrener_Maar_30.jpg"></ListItem>
     );
     itemList.push(
       <ListItem Title="Title" Description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse placerat pulvinar ante pretium pretium. Quisque mollis a urna ut accumsan. Maecenas ut mattis tellus. Cras eu congue metus, sit amet pulvinar nunc. Cras dignissim lacus nec bibendum bibendum. Sed ipsum ex, imperdiet nec metus vitae, maximus ultrices metus. Duis posuere pellentesque nibh vitae molestie. Donec ullamcorper bibendum tempor. Nunc massa leo, placerat in nisi vitae, bibendum pellentesque eros. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae." ImageSource="https://upload.wikimedia.org/wikipedia/commons/e/ed/20170522_Schalkenmehrener_Maar_30.jpg"></ListItem>
@@ -63,16 +63,21 @@ function ItemList(props) {
 
 function ListItem(props) {
   var description = props.Description;
+  var title = props.Title;
 
   if(props.Description.length > 140) {
     description = props.Description.slice(0, 140) + "...";
+  }
+
+  if(props.Title.length > 20) {
+    title = props.Title.slice(0, 20) + "...";
   }
 
   return (
     <div class="ListItem">
       <img src={ props.ImageSource }></img>
       <div>
-        <p class="ListItemTitle">{ props.Title }</p>
+        <p class="ListItemTitle">{ title }</p>
         <p class="ListItemDescription">{ description }</p>
       </div>
     </div>
